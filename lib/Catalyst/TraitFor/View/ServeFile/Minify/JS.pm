973e270e9b9ebf28df8fc87c877eafae55ddb896
ijw <ijw@vm3549.vps.tagadab.com>
2010-03-09 14:30:35 +0000

    JS minifier as a trait

--- lib/Catalyst/TraitFor/View/ServeFile/Minify/JS.pm
+++ lib/Catalyst/TraitFor/View/ServeFile/Minify/JS.pm
@@ -3,113 +3,33 @@ package Catalyst::View::JavaScript::Minifier::XS;
 # ABSTRACT: Minify your served JavaScript files
 
 use autodie;
-use Moose;
-extends 'Catalyst::View';
+use Moose::Role;
+requires 'Catalyst::View::ServeFile';
 
 use JavaScript::Minifier::XS qw/minify/;
-use Path::Class::File;
-use URI;
 
-has stash_variable => (
-   is => 'ro',
-   isa => 'Str',
+has '+stash_variable' => (
    default => 'js',
 );
 
-has path => (
-   is => 'ro',
-   isa => 'Str',
+has '+path' => (
    default => 'js',
 );
 
-has subinclude => (
-   is => 'ro',
-   isa => 'Bool',
-   default => undef,
+has '+default_content_type' => (
+   default => 'text/javascript',
 );
 
-sub process {
-   my ($self,$c) = @_;
-
-   my $original_stash = $c->stash->{$self->stash_variable};
-   my @files = $self->_expand_stash($original_stash);
-
-   $c->res->content_type('text/javascript');
-
-   push @files, $self->_subinclude($c, $original_stash, @files);
-
-   my $home = $self->config->{INCLUDE_PATH} || $c->path_to('root');
-   @files = map {
-      $_ =~ s/\.js$//;
-      Path::Class::File->new( $home, $self->path, "$_.js" );
-   } grep { defined $_ && $_ ne '' } @files;
-
-   my @output = $self->_combine_files($c, \@files);
-
-   $c->res->body( $self->_minify($c, \@output) );
-}
-
-sub _subinclude {
-   my ( $self, $c, $original_stash, @files ) = @_;
-
-   return unless $self->subinclude && $c->request->headers->referer;
-
-   unless ( $c->request->headers->referer ) {
-      $c->log->debug('javascripts called from no referer sending blank');
-      $c->res->body( q{ } );
-      $c->detach();
-   }
-
-   my $referer = URI->new($c->request->headers->referer);
-
-   if ( $referer->path eq '/' ) {
-      $c->log->debug(q{we can't take js from index as it's too likely to enter an infinite loop!});
-      return;
-   }
-
-   $c->forward('/'.$referer->path);
-   $c->log->debug('js taken from referer : '.$referer->path);
-
-   return $self->_expand_stash($c->stash->{$self->stash_variable})
-      if $c->stash->{$self->stash_variable} ne $original_stash;
-}
-
-sub _minify {
+around filter => sub {
    my ( $self, $c, $output ) = @_;
 
-   if ( @{$output} ) {
+   if ( defined($output) ) {
       return $c->debug
-         ? join q{ }, @{$output}
-         : minify(join q{ }, @{$output} )
+         ? $output
+         : minify($output);
    } else {
       return q{ };
    }
-}
-
-sub _combine_files {
-   my ( $self, $c, $files ) = @_;
-
-   my @output;
-   for my $file (@{$files}) {
-      $c->log->debug("loading js file ... $file");
-      open my $in, '<', $file;
-      for (<$in>) {
-         push @output, $_;
-      }
-      close $in;
-   }
-   return @output;
-}
-
-sub _expand_stash {
-   my ( $self, $stash_var ) = @_;
-
-   if ( $stash_var ) {
-      return ref $stash_var eq 'ARRAY'
-         ? @{ $stash_var }
-	 : split /\s+/, $stash_var;
-   }
-
 }
 
 1;
@@ -118,8 +38,9 @@ sub _expand_stash {
 
 =head1 SYNOPSIS
 
- # creating MyApp::View::JavaScript
- ./script/myapp_create.pl view JavaScript JavaScript::Minifier::XS
+ View::JSMini => { # (a StashFile view)
+   traits => qw/Minifier::JS/
+ }
 
  # in your controller file, as an action
  sub js : Local {
@@ -128,7 +49,7 @@ sub _expand_stash {
     # loads root/js/script1.js and root/js/script2.js
     $c->stash->{js} = [qw/script1 script2/];
 
-    $c->forward('View::JavaScript');
+    $c->forward('View::JSMini');
  }
 
  # in your html
@@ -154,30 +75,12 @@ sets a different path for your javascript files
 
 default : js
 
-=item subinclude
-
-setting this to true will take your js files (stash variable) from your referer
-action
-
- # in your controller
- sub action : Local {
-    my ( $self, $c ) = @_;
-
-    # load exclusive.js only when /action is loaded
-    $c->stash->{js} = "exclusive";
- }
-
-This could be very dangerous since it's using
-C<< $c->forward($c->request->headers->referer) >>. It doesn't work with the
-index action!
-
-default : false
-
 =back
 
 =cut
 
 =head1 SEE ALSO
 
+L<Catalyst::View::ServeFile>
 L<JavaScript::Minifier::XS>
 
